8b468d5a0e60cc8580ef336f58b231ce56fd5d07
Anthony P. Alicea <anthonypalicea@gmail.com>
2012-11-20 17:32:45 -0500

    Initial commit

---
+++ Scripts/bootout.js
@@ -0,0 +1,56 @@
+﻿/*!
+* Bootout.js by Anthony P. Alicea
+* http://www.anthonypalicea.com
+*
+* A collection of Knockout bindings for Bootstrap's 13 jQuery plugins
+* Depends on jQuery, Twitter Bootstrap, and Knockout JS
+*
+* http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
+
+
+/* Modals */
+
+
+
+/* Dropdown */
+
+
+
+/* Scrollspy */
+
+
+
+/* Tab */
+
+
+
+/* Tooltip */
+
+
+
+/* Popover */
+
+
+
+/* Alert */
+
+
+
+/* Button */
+
+
+
+/* Collapse */
+
+
+
+/* Carousel */
+
+
+
+/* Typeahead */
+
+
+
+/* Affix */
